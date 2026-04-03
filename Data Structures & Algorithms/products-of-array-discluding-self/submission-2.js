ef148b27382bb2957968a0d2e441bb620d75d8e0
Pawan Kuntal <pawankuntal833@gmail.com>
2026-04-04 01:46:35 +0530

    Add: products-of-array-discluding-self - submission-2

---
+++ Data Structures & Algorithms/products-of-array-discluding-self/submission-2.js
@@ -0,0 +1,21 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number[]}
+     */
+    productExceptSelf(nums) {
+        let n = nums.length
+        let ans = new Array(n).fill(1)
+
+        for(let i =1; i<n; i++){
+            ans[i] = ans[i-1]* nums[i-1]
+        }
+
+        let suffix = 1
+        for(let i = n-2; i>=0; i--){
+            suffix *= nums[i+1]
+            ans[i] *= suffix
+        }
+        return ans
+    }
+}
