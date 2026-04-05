6f6c7b840eb0155354969db22affe8634c2273e9
Pawan Kuntal <pawankuntal833@gmail.com>
2026-04-06 02:55:29 +0530

    Add: intersection-of-two-arrays - submission-0

---
+++ Data Structures & Algorithms/intersection-of-two-arrays/submission-0.js
@@ -0,0 +1,18 @@
+class Solution {
+    /**
+     * @param {number[]} nums1
+     * @param {number[]} nums2
+     * @return {number[]}
+     */
+    intersection(nums1, nums2) {
+        let set = new Set(nums1)
+        let ans = []
+
+        for(let i =0; i<nums2.length; i++){
+            if(set.has(nums2[i]) && !ans.includes(nums2[i])){
+                ans.push(nums2[i])
+            }
+        }
+        return ans
+    }
+}
